081be7b66ca80c134f7c816c5b84766e66022426
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-02-03 20:53:27 +0530

    feat: zustand

--- app/zustland/page.tsx
+++ app/zustland/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
+import { useCounterStore } from "@/lib/store";
 
 const Zustexample = () => {
-  return <div>Zustexample</div>;
+  const count = useCounterStore((state) => state.count);
+
+  return (
+    <div className="flex justify-center items-center h-screen flex-col">
+      <h1 className="text-4xl font-semibold">Zustland</h1>
+      <div>Count: {count}</div>
+    </div>
+  );
 };
 
 export default Zustexample;
